47fdefcb7b30908e389d9fa19efd10270b07e7c9
mthus2006 <166153564+mthus2006@users.noreply.github.com>
2024-11-18 14:06:07 -0300

    y

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao{
+    'Aleatório',
+    'Quem roubou mais que o Lula?',
+    'Ninguém'
+}
+
+criaCartao{
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris'
+}
+
+criaCartao{
+    'Geografia',
+    'Qual é a capital da Alemanha?',
+    'Berlim'
+}
+
+criaCartao{
+    'Geografia',
+    'Qual é a capital da França?',
+    'Paris'
+}
+
+criaCartao{
+    'Aleatório',
+    'Quem matou Michael Jackson?',
+    'As drogas'
+}
+
+criaCartao{
+    'Aleatório',
+    'Quem matou o Pelé?',
+    'As drogas'
+}
